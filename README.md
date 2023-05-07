127b58a57bb83de6a13a655e0b3d210f76c78656
xPumaa <35280763+xPumaa@users.noreply.github.com>
2023-05-07 03:55:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Slice Is Right
+Slice Is Right is a FFXIV Dalamud plugin that shows where Bamboo falls.
+
+## Installation
+```
+https://raw.githubusercontent.com/xPumaa/DalamudPlugins/main/pluginmaster.json
+```
+* Copy the link above
+* Open your Plugin Installer Window and click Settings
+* Go to the Experimental Tab
+* Paste the link into the Custom Plugin Repositories, and click the + on the right to add it.
+* Save and close.
+* Search and Install `Slice Is Right` from the Plugin Installer.
+* Enjoy
---
+++ SliceIsRight/Services.cs
@@ -0,0 +1,15 @@
+﻿using Dalamud.Game.ClientState;
+using Dalamud.Game.ClientState.Objects;
+using Dalamud.Game.Gui;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+
+namespace SliceIsRight;
+
+public class Services
+{
+    [PluginService] public static DalamudPluginInterface PluginInterface { get; private set; } = null!;
+    [PluginService] public static ObjectTable ObjectTable { get; private set; } = null!;
+    [PluginService] public static ClientState ClientState { get; private set; } = null!; 
+    [PluginService] public static GameGui GameGui { get; private set; } = null!;
+}
